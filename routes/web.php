256e7fd2f1e852a87398116e7e54ae55124836ed
Administrator <sonvt.vietmmo@gmail.com>
2025-03-13 09:28:50 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Filament\Pages\PublicCreateDangKy;
 use App\Filament\Pages\PublicCreateTongHop;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +18,9 @@ Route::get('/dk', PublicCreateDangKy::class)
     ->name('dang_ky')
     ->middleware('web');
 
+Route::get('/clear',function (){
+    echo  Artisan::call('optimize:clear');
+    echo  Artisan::call('cache:clear');
+    echo  Artisan::call('config:cache');
+    echo  Artisan::call('route:cache');
+});
